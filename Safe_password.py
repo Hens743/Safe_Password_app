adcac73c1bf144450cc592b014e19de73401ec7a
Hens743 <80003512+Hens743@users.noreply.github.com>
2024-05-06 11:56:54 +0200

    Update Safe_password.py

--- Safe_password.py
+++ Safe_password.py
@@ -4,25 +4,29 @@ import string
 import math
 import qrcode
 
-# Constants for character sets
-LETTERS = string.ascii_letters
-DIGITS = string.digits
-PUNCTUATION = string.punctuation
-SPECIALS = "!@#$%^&*()_+-=[]{}|;:,.<>?/~`£µ" 
-SCANDINAVIAN = "åäöÅÄÖåÅäÄöÖøØæÆ"
-ICELANDIC = "áÁðÐéÉíÍóÓúÚýÝþÞ"
-
-def calculate_entropy(length, character_set):
-    """Calculates the entropy of a password given its length and character set."""
-    if not character_set:
+def calculate_entropy(length, include_letters=True, include_digits=True, include_punctuation=True, include_specials=False, include_scandinavian=False, include_icelandic=False):
+    characters = ""
+    if include_letters:
+        characters += string.ascii_letters
+    if include_digits:
+        characters += string.digits
+    if include_punctuation:
+        characters += string.punctuation
+    if include_specials:
+        characters += "!@#$%^&*()_+-=[]{}|;:,.<>?/~`£µ"
+    if include_scandinavian:
+        characters += "åäöÅÄÖåÅäÄöÖøØæÆ"  # Scandinavian characters
+    if include_icelandic:
+        characters += "áÁðÐéÉíÍóÓúÚýÝþÞ"  # Icelandic characters
+    
+    if not characters:
         return 0
-
-    n = len(character_set)  # Number of possible characters
-    entropy = math.log2(n ** length)
+    
+    N = len(characters)
+    entropy = math.log2(N ** length)
     return round(entropy)
 
 def estimate_strength(entropy):
-    """Estimates password strength based on its entropy."""
     if entropy < 28:
         return "Very Weak"
     elif entropy < 56:
@@ -34,30 +38,28 @@ def estimate_strength(entropy):
     else:
         return "Very Strong"
 
-def generate_password(length, character_set):
-    """Generates a secure random password."""
-    if not character_set:
+def generate_password(length, include_letters=True, include_digits=True, include_punctuation=True, include_specials=False, include_scandinavian=False, include_icelandic=False):
+    characters = ""
+    if include_letters:
+        characters += string.ascii_letters
+    if include_digits:
+        characters += string.digits
+    if include_punctuation:
+        characters += string.punctuation
+    if include_specials:
+        characters += "!@#$%^&*()_+-=[]{}|;:,.<>?/~`£µ"
+    if include_scandinavian:
+        characters += "åäöÅÄÖåÅäÄöÖøØæÆ"  # Scandinavian characters
+    if include_icelandic:
+        characters += "áÁðÐéÉíÍóÓúÚýÝþÞ"  # Icelandic characters
+    
+    if not characters:
         st.error("Please select at least one character type.")
-        return None  # Indicate failure
-
-    password = ''.join(secrets.choice(character_set) for _ in range(length))
+        return
+    
+    password = ''.join((secrets.choice(characters) for _ in range(length)))
     return password
 
-def display_qr_code(data):
-    """Displays a QR code for the given data."""
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
-    qr.add_data(data)
-    qr.make(fit=True)
-
-    img = qr.make_image(fill_color="black", back_color="white")
-    st.image(img, caption="QR Code", use_column_width=True)
-
-# Streamlit UI
 st.title("Secure Password Generator")
 
 # Options for generating the password
@@ -69,39 +71,34 @@ include_specials = st.checkbox("Include Special Characters", value=False)
 include_scandinavian = st.checkbox("Include Scandinavian Characters", value=False)
 include_icelandic = st.checkbox("Include Icelandic Characters", value=False)
 hide_password = st.checkbox("Hide Password", value=False)
-show_qr_code = st.checkbox("Show QR Code", value=False)
+display_qr_code = st.checkbox("Display as QR Code", value=False)
 
 if st.button("Generate Password"):
-    character_set = ""
-    if include_letters:
-        character_set += LETTERS
-    if include_digits:
-        character_set += DIGITS
-    if include_punctuation:
-        character_set += PUNCTUATION
-    if include_specials:
-        character_set += SPECIALS
-    if include_scandinavian:
-        character_set += SCANDINAVIAN
-    if include_icelandic:
-        character_set += ICELANDIC
-
-    entropy = calculate_entropy(length, character_set)
-    password = generate_password(length, character_set)
-
+    entropy = calculate_entropy(length, include_letters, include_digits, include_punctuation, include_specials, include_scandinavian, include_icelandic)
+    password = generate_password(length, include_letters, include_digits, include_punctuation, include_specials, include_scandinavian, include_icelandic)
     if password:
         strength = estimate_strength(entropy)
         if hide_password:
             password_display = "*" * len(password)
         else:
             password_display = password
-
         data = {"Generated Password": password_display, "Strength": strength, "Entropy (bits)": entropy}
         st.table(data)
+        if display_qr_code:
+            qr = qrcode.QRCode(version=1, box_size=10, border=5)
+            qr.add_data(password)
+            qr.make(fit=True)
+            img = qr.make_image(fill_color="black", back_color="white")
+            st.image(img, caption="Password QR Code", use_column_width=True)
 
-        if show_qr_code:
-            display_qr_code(password)
-
-# Recommended guidelines (Sidebar)
+# Recommended guidelines
 st.sidebar.markdown("### Common Guidelines")
-# ... (Your guidelines text)
+st.sidebar.markdown("""
+- Consider a minimum password length of 8 characters as a general guide. Both the US and UK cyber security departments recommend long and easily memorable passwords over short complex ones.
+- Generate passwords randomly where feasible.
+- Avoid using the same password twice (e.g. across multiple user accounts and/or software systems).
+- Avoid character repetition, keyboard patterns, dictionary words, and sequential letters or numbers.
+- Avoid using information that is or might become publicly associated with the user or the account, such as the user name, ancestors' names, or dates.
+- Avoid using information that the user's colleagues and/or acquaintances might know to be associated with the user, such as relatives or pet names, romantic links (current or past), and biographical information (e.g. ID numbers, ancestors' names or dates).
+- Do not use passwords that consist wholly of any simple combination of the aforementioned weak components.
+""")
